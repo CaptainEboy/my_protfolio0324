c5626ca3337704662b8272ef9165aad3a2bc0b37
CaptainEboy <emaapps.ac24@gmail.com>
2024-03-30 19:41:07 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+// Reference Link, How To Install Tailwindcss
+// https://tailwindcss.com/docs/guides/create-react-app
+// https://medium.com/@shivaydv/a-step-by-step-guide-to-installing-tailwind-css-with-react-b06d71a50354
+// Tutorial Link
+// https://www.freecodecamp.org/news/build-portfolio-website-react/
 
-function App() {
+import React from "react";
+import './App.css';
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Testimonials from "./components/Testimonials";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
   );
 }
 
-export default App;
+// export default function App() {
+//   return (
+//     <h1 className="text-3xl font-bold underline">
+//       Hello world!
+//     </h1>
+//   )
+// }
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/data/data.js
@@ -0,0 +1,96 @@
+// src/data.js
+// import easylender from '../assets/easylender.png';
+
+export const projects = [
+    {
+      title: "React Reserve",
+      subtitle: "MERN Stack",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: require('../assets/easylender.png'),
+      link: "https://reactbootcamp.com",
+    },
+    {
+      title: "React Tracks",
+      subtitle: "React and Python",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image:  require('../assets/ooutradehub.png'),
+      link: "https://reedbarger.com",
+    },
+    {
+      title: "DevChat",
+      subtitle: "React and Firebase",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image:  require('../assets/yoo.png'),
+      link: "https://jsbootcamp.com",
+    },
+    {
+      title: "Epic Todo App",
+      subtitle: "React Hooks",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image:  require('../assets/emacomplier.png'),
+      link: "https://pythonbootcamp.com",
+    },
+  ];
+
+
+
+  export const testimonials = [
+    {
+      name: "React Reserve",
+      company: "MERN Stack",
+      quote:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: require('../assets/easylender.png'),
+      link: "https://reactbootcamp.com",
+    },
+    {
+      name: "React Tracks",
+      company: "React and Python",
+      quote:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image:  require('../assets/ooutradehub.png'),
+      link: "https://reedbarger.com",
+    },
+    {
+      name: "DevChat",
+      company: "React and Firebase",
+      quote:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image:  require('../assets/yoo.png'),
+      link: "https://jsbootcamp.com",
+    },
+    {
+      name: "Epic Todo App",
+      company: "React Hooks",
+      quote:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image:  require('../assets/emacomplier.png'),
+      link: "https://pythonbootcamp.com",
+    },
+  ];
+
+
+
+
+  export const skills = [
+    {
+      name: "React Reserve",
+      
+    },
+    {
+      name: "React Tracks",
+     
+    },
+    {
+      name: "DevChat",
+      
+    },
+    {
+      name: "Epic Todo App",
+     
+    },
+  ];
